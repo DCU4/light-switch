9fd24f33f4776bd6410148d2272792fbbc3f372e
Dylan Connor <DylanJConnor4@gmail.com>
2020-03-28 20:58:12 -0400

    typo

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
 
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
-var io = require('socket.io').listen(server);//require socket.io module and pass the http object (server)
+var io = require('socket.io').listen(http);//require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED04 = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var LED06 = new Gpio(6, 'out'); //use GPIO pin 4 as output
