558be84492b88e61226591cce5fa54a7472813a5
Dylan Connor <DylanJConnor4@gmail.com>
2020-03-29 00:04:13 -0400

    rework websocket

--- app.js
+++ app.js
@@ -21,7 +21,8 @@
 
 
 var server = require('http').createServer(handler); //require http server, and create server with function handler()
-var io = require('socket.io').listen(server);//require socket.io module and pass the http object (server)
+// var io = require('socket.io').listen(server);//require socket.io module and pass the http object (server)
+const io = socketIO(server);
 var fs = require('fs'); //require filesystem module
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED04 = new Gpio(4, 'out'); //use GPIO pin 4 as output
