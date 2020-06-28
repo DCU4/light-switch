91b2ee9dada5be7b57201e46aae3587e906a32fb
Dylan Connor <DylanJConnor4@gmail.com>
2020-06-28 16:06:13 -0400

    setting up switch on audio

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-var server = require('http').createServer(handler); //require http server, and create server with function handler()
-var io = require('socket.io').listen(server);//require socket.io module and pass the http object (server)
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED04 = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var LED06 = new Gpio(6, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 //Put all the LED variables in an array
 var leds = [LED04, LED06];
+
+var audioIn = new Gpio(18, 'in', 'both', {debounceTimeout: 100});
+
 var indexCount = 0; //a counter
 
-server.listen(process.env.PORT || 3000, function(){
-  console.log('Server started',process.env.PORT );
-});
-
-
-// server.listen(process.env.PORT || 3000);
-// http.listen(8080); //listen to port 8080
+http.listen(3000); //listen to port 8080
 
 function handler (req, res) { //create server
   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
@@ -39,12 +36,40 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
     lightvalue = value;
     socket.emit('light', lightvalue); //send button status to client
   });
+  
   socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
+    console.log('data onlight',data)
+    lightvalue = (data);
     leds.forEach((l,e) => {
-      console.log(l);
+      // console.log(l);
       if (lightvalue != l.readSync()) { //only change LED if status has changed
         l.writeSync(lightvalue); //turn LED on or off
+      }
+    });
+  });
+
+
+  // audio 
+  var audiovalue = 0;
+  audioIn.watch(function (err, value) { 
+    if (err) { //if an error
+      console.error('There was an error', err); //output error message to console
+      return;
+    }
+    console.log('watch', value);
+    audiovalue = value;
+    socket.emit('audio', audiovalue); //send button status to client
+  });
+
+  socket.on('audio', function(data) { //get light switch status from client
+    console.log('data onaudio',data)
+    audiovalue = data;
+    leds.forEach((l,e) => {
+
+      audiovalue = (data == l.readSync() ? 0 : 1);
+      console.log(audiovalue, l.readSync());
+      if (audiovalue != l.readSync()) { //only change LED if status has changed
+        l.writeSync(audiovalue); //turn LED on or off
       }
     });
   });
@@ -58,4 +83,3 @@ process.on('SIGINT', function () { //on ctrl+c
     process.exit(); //exit completely
   });
 });
-
